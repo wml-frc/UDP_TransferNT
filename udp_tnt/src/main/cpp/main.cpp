dfd4fc0750aa78aedadc337d2022b646036aee0e
CJBuchel <connorbuchel@gmail.com>
2021-03-17 00:26:44 +0800

    Added buildable structure

---
+++ udp_tnt/src/main/cpp/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+// Server headers
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <string.h> 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <netinet/in.h> 
+
+#define PORT    8080
+#define MAXLINE 1024
+
+
+
+int main() {
+	std::cout << "Hello world" << std::endl;
+	return 0;
+}
